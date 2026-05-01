e388b8b294692241fc5112c4f592aad455963fa8
gusza110811 <auguz1108@gmail.com>
2026-05-02 03:55:23 +0700

    yeahh

---
+++ main.py
@@ -0,0 +1,138 @@
+class Compiler:
+    def __init__(self, source:str):
+        self.open = 0
+        self.close = 0
+        self.depth = 0
+        self.index = 0
+        self.source = source
+        self.length = len(self.source)
+    def get(self):
+        if self.length == self.index:
+            return False
+
+        val = self.source[self.index]
+        self.index += 1
+        return val
+
+    def main(self):
+        output:list[str] = [
+            ".offset 0x7c00",
+            "func main {",
+            " mov cx, 0x2000",
+            " mov ds, cx",
+            " mov dx, 1",
+            " mov bx, 0",
+        ]
+
+        # main gen
+        while 1:
+            char = self.get()
+            if not char: break
+
+            match char:
+                case "-":
+                    output.append(" sub ax, 1")
+                case "+":
+                    output.append(" add ax, 1")
+
+                case "[":
+                    output.append(f"open_{self.open}:")
+                    output.append(" mov ah, 0")
+                    output.append(f" cmp ax, 0")
+                    output.append(f" jz close_{self.open}")
+                    self.depth += 1
+                    self.open += 1
+                case "]":
+                    self.depth -= 1
+                    output.append(f" jmp open_{self.close}")
+                    output.append(f"close_{self.close}:")
+                    self.close += 1
+                
+                case "<":
+                    output.append(" mov [b bx], ax")
+                    output.append(" sub bx, 1")
+                    output.append(" mov ax, [b bx]")
+                case ">":
+                    output.append(" mov [b bx], ax")
+                    output.append(" add bx, 1")
+                    output.append(" mov ax, [b bx]")
+                
+                case ".":\
+                    output.append(" int 0x14")
+
+        output.extend([
+            " hlt",
+            "}"
+        ])
+
+        def next_line(idx):
+            if idx+1 < len(output):
+                return output[idx+1]
+            else:
+                return ""
+
+        optimized = []
+        # optimize pointer
+        for idx, line in enumerate(output):
+            next = next_line(idx)
+            if line == " mov ax, [b bx]" and next == " mov [b bx], ax":
+                continue
+            else:
+                optimized.append(line)
+        output = optimized
+
+        optimized = []
+        # optimize addition and subtraction
+        idx = 0
+        while idx < len(output):
+            line = output[idx]
+            next = next_line(idx)
+
+            if line == " add ax, 1":
+                count = 1
+                while next == line:
+                    count += 1
+                    idx += 1
+                    next = next_line(idx)
+                optimized.append(f" add ax, {count}")
+                idx += 1
+            elif line == " add bx, 1":
+                count = 1
+                while next == line:
+                    count += 1
+                    idx += 1
+                    next = next_line(idx)
+                optimized.append(f" add bx, {count}")
+                idx += 1
+            elif line == " sub ax, 1":
+                count = 1
+                while next == line:
+                    count += 1
+                    idx += 1
+                    next = next_line(idx)
+                optimized.append(f" sub ax, {count}")
+                idx += 1
+            elif line == " sub bx, 1":
+                count = 1
+                while next == line:
+                    count += 1
+                    idx += 1
+                    next = next_line(idx)
+                optimized.append(f" sub bx, {count}")
+                idx += 1
+            else:
+                idx += 1
+                optimized.append(line)
+        output = optimized
+
+        return "\n".join(output)
+
+if __name__ == "__main__":
+    test = "-[------->+<]>-.-[->+++++<]>++.+++++++..+++.[->+++++<]>+.------------.---[->+++<]>.-[--->+<]>---.+++.------.--------.-[--->+<]>."
+
+    compiler = Compiler(test)
+
+    output = compiler.main()
+
+    with open("out.asm","w") as out:
+        out.write(output)
